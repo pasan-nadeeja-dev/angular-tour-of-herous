42b11e8d404b2c1763cc670d8162f86640217822
Pasan Nadeeja <psaan.nadeeja@my.sliit.lk>
2018-07-15 04:29:45 +0530

    Add hero service component for access mock data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "./hero";
-import { HEROES } from "../mock-heroes";
+// import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -16,18 +17,24 @@ export class HeroesComponent implements OnInit {
   // };
 
   // property HEROES
-  heroes = HEROES;
+  heroes : Hero[];
 
   //property selected hero
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   // <li> click event handler
   onSelect(hero: Hero) : void {
     this.selectedHero = hero;
+  }
+
+  //call below function to get data from service
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
